b4971617d91317f22427d1b8cd2a093f921447e8
Your Name <you@example.com>
2021-11-24 09:49:30 +0300

    hw

---
+++ HW_week7Application/app/src/main/java/com/example/hw_week7application/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.hw_week7application
+
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkLocationPermission()
+    }
+
+    fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            //show request permission dialog
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                1
+            )
+
+
+        } else {
+            var worker = PeriodicWorkRequestBuilder<LocationWorker>(10, TimeUnit.SECONDS).build()
+            WorkManager.getInstance(this).enqueue(worker)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            checkLocationPermission()
+        } else {
+            AlertDialog.Builder(this).apply {
+                title = "Warning"
+                setMessage("To access location go the setting and allow location service")
+                setPositiveButton("Ok", { dialog, which ->
+                })
+            }.show()
+        }
+    }
+}
